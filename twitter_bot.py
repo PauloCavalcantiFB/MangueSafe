5f7ea370a0da3e27284c23498d4ad9ae09f50306
PauloCavalcantiFB <cavalcanti.paulofb@gmail.com>
2020-10-26 20:42:30 -0300

    Script de recorte de textos grandes para tweets

--- twitter_bot.py
+++ twitter_bot.py
@@ -7,13 +7,53 @@ auth.set_access_token("1048807349434155008-wPDPwnTPm5EmOOKZEeidQFFK9MUwZc", "0cG
 #Cria um objeto API
 api = tweepy.API(auth)
 
+raw = input("Insert the text: ")
+input_raw = list(raw)
+commit = []
+tweets = []
+temp = ''
+flag = True
+contador = 0
+
+for c in range(len(input_raw)):
+	if c == 0:
+		commit.append(input_raw[c])
+	elif c % 130 != 0:
+		commit.append(input_raw[c])
+	elif c % 130 == 0 and flag == False:
+		commit.append(input_raw[c])
+		temp = temp.join(commit)
+		#print(temp)
+		tweets.append(api.update_status(status=temp, in_reply_to_status_id=tweets[contador].id, auto_populate_reply_metadata=True))
+		contador += 1
+		commit.clear()
+		temp = ''
+	elif c % 130 == 0 and flag == True:
+		commit.append(input_raw[c])
+		temp = temp.join(commit)
+		#print(temp)
+		tweets.append(api.update_status(temp))
+		commit.clear()
+		temp = ''
+		flag = False
+temp = temp.join(commit)
+tweets.append(api.update_status(status=temp, in_reply_to_status_id=tweets[contador].id, auto_populate_reply_metadata=True))
+
+#if len(tweets) > 1:
+#	original = api.update_status(tweets[0])
+#	for i in range(len(tweets)):
+#		original = api.update_status(tweets[i + 1], in_reply_to_status_id=original.id, auto_populate_reply_metadata=True)
+
 #Cria um tweet
-original_tweet = api.update_status('teste de thread3')
-txt_thread = 'Reply update_satus original.'
-api.update_status(status=txt_thread, in_reply_to_status_id = original_tweet.id, auto_populate_reply_metadata=True)
+#original_tweet = api.update_status('teste de thread3')
+#txt_thread = 'Reply update_satus original.'
+#api.update_status(status=txt_thread, in_reply_to_status_id = original_tweet.id, auto_populate_reply_metadata=True)
 
 #try:
 #    api.verify_credentials()
 #    print("Autenticação OK.")
 #except:
 #    print("Ocorreu algum erro.")
+
+
+#The word “Calligraphy” is derived from Greek, meaning “beautiful writing”. Calligraphy or the art of fancy writing has thousands of years in its history and development. They are of aesthetics, refinement, creativity and pure beauty.
